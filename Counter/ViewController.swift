7892a2a37b6b2fbcefb0464bfa3029a211041440
Luba <sapufira@gmail.com>
2024-06-11 21:23:59 -0700

    done

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var label: UILabel!
+    var count = 0
+    
+    
+    func updateLabel(count:Int){
+        label.text="\(count)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateLabel(count: count)
     }
-
-
+    
+    @IBAction func onClick(_ sender: Any) {
+        count+=1
+        print(count)
+        // label.text="\(count)"
+        updateLabel(count: count)
+    }
+    
 }
 
